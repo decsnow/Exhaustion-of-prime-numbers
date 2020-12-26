c4a16cb31f73cb695167eb9408bba038dcb95665
decsnow <61638919+decsnow@users.noreply.github.com>
2020-12-26 23:53:00 +0800

    Add files via upload

---
+++ 质数穷举.py
@@ -0,0 +1,17 @@
+import time
+n=0
+x=input('type the range')
+start=time.time()
+for i in range(2,int(x)):
+    j=2
+    m=i**0.5
+    while j<=m:
+        if i%j==0:
+            break
+        j+=1
+    else:
+        print(i)
+        n=n+1
+stop=time.time()        
+print('the number is',n)
+print('it has cost',stop-start,'s')        
