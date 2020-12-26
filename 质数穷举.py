c10d71ac72959917dd9b7ba637fb9d9609f07c15
decsnow <61638919+decsnow@users.noreply.github.com>
2020-12-27 00:27:07 +0800

    Update 质数穷举.py

--- 质数穷举.py
+++ 质数穷举.py
@@ -4,7 +4,7 @@ x=input('type the range')
 start=time.time()
 for i in range(2,int(x)):
     j=2
-    m=i**0.5
+    m=i**0.5 #core:Set the upper limit of the enumeration to the radical i
     while j<=m:
         if i%j==0:
             break
